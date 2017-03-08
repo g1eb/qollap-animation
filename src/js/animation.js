ad6a5573c648deb8ebd2dad1a823907ece612559
Gleb <mail@g1eb.com>
2017-03-08 18:18:33 +0100

    Added initial setup js and styles.

--- src/js/animation.js
+++ src/js/animation.js
@@ -2,16 +2,37 @@
 
 var app = {
 
+  items: new Array(15),
+
   init: function () {
     app.container = document.createElement('div');
     app.container.className = 'container';
     document.body.appendChild(app.container);
+
+    for ( var i = 0; i < app.items.length; i++ ) {
+      app.add(app.items[i]);
+    }
   },
 
   add: function () {
+    var element = document.createElement('div');
+    element.classList.add('hidden');
+    app.container.appendChild(element);
+
+    var timeout = Math.round(Math.random() * 1000);
+    window.setTimeout(app.show, timeout, element);
   },
 
-  animate: function () {
+  show: function (element) {
+    element.classList.remove('hidden');
+    app.animate(element);
+  },
+
+  animate: function (element) {
+    var timeout = Math.ceil(Math.random() * 10) * 1000;
+    window.setTimeout(function (element) {
+      app.animate(element);
+    }, timeout, element);
   },
 
 };
